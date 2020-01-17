0915ac08f9ff5aa295466bbe3fb3e39c8ec02d02
jtonello <john@johntonello.com>
2020-01-17 13:28:53 -0500

    Added comments.

--- prometheus/start.sh
+++ prometheus/start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Copy the environment variable to a file
 echo $TARGETS > targets
+
+# Clean up the entries to parse correctly
 sed -i "s/ //g" targets
 sed -ie "s/^/\\\'/g" targets
 sed -ie "s/$/\\\'/g" targets
 sed -ie "s/,/\\\',\\\'/g" targets
 
+# Replace the default node_exporter entry with all the targets
 sed -ie "s/\['node_exporter:9100'\]/TARGETS/g" /etc/prometheus/prometheus.yml
-
 sed -ie "s/TARGETS/\['node_exporter:9100',$(cat targets)\]/g" /etc/prometheus/prometheus.yml
 
+# Start Prometheus with the updated config file
 ./prometheus/prometheus --config.file=/etc/prometheus/prometheus.yml
