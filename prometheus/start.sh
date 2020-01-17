ff06985d1589b143b73448d37d97b3b4317e759c
jtonello <john@johntonello.com>
2020-01-17 16:43:14 -0500

    Node exporter fixes.

--- prometheus/start.sh
+++ prometheus/start.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Copy the environment variable to a file
-echo $TARGETS > targets
+if [[ ! -z $TARGETS ]]
+then
+    # Copy the environment variable to a file
+    echo $TARGETS > targets
 
-# Clean up the entries to parse correctly
-sed -i "s/ //g" targets
-sed -ie "s/^/\\\'/g" targets
-sed -ie "s/$/\\\'/g" targets
-sed -ie "s/,/\\\',\\\'/g" targets
+    # Clean up the entries to parse correctly
+    sed -i "s/ //g" targets
+    sed -ie "s/^/\\\'/g" targets
+    sed -ie "s/$/\\\'/g" targets
+    sed -ie "s/,/\\\',\\\'/g" targets
 
-# Replace the default node_exporter entry with all the targets
-sed -ie "s/\['node_exporter:9100'\]/TARGETS/g" /etc/prometheus/prometheus.yml
-sed -ie "s/TARGETS/\['node_exporter:9100',$(cat targets)\]/g" /etc/prometheus/prometheus.yml
+    # Replace the default node_exporter entry with all the targets
+    sed -ie "s/\['node_exporter:9100'\]/TARGETS/g" /etc/prometheus/prometheus.yml
+    sed -ie "s/TARGETS/\['node_exporter:9100',$(cat targets)\]/g" /etc/prometheus/prometheus.yml
+fi
 
 # Start Prometheus with the updated config file
+chmod a+x ./prometheus/prometheus
 ./prometheus/prometheus --config.file=/etc/prometheus/prometheus.yml
